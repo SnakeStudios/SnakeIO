a54667448af9976c2ea3e6d5006bbd9a63b55942
iSnakeBuzz <contacto@isnakebuzz.com>
2020-08-03 07:35:05 -0300

    Smooth

--- src/main/java/com/voteservers/snakeio/events/system/Event.java
+++ src/main/java/com/voteservers/snakeio/events/system/Event.java
@@ -4,6 +4,7 @@ import com.voteservers.snakeio.SnakeIO;
 import com.voteservers.snakeio.Utils.Data;
 import lombok.SneakyThrows;
 
+import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
 
 @lombok.Data
@@ -11,14 +12,17 @@ public class Event {
 
     private boolean cancelled;
 
-    @SneakyThrows
     public void callEvent() {
         Event event = this;
         ArrayList<Data> dataList = SnakeIO.getEventsManager().getData(event.getClass());
         if (dataList != null && !dataList.isEmpty()) {
             dataList.forEach(data -> {
                 // Calling event
-                data.getMethod().invoke(data.getClazz(), event);
+                try {
+                    data.getMethod().invoke(data.getClazz(), event);
+                } catch (IllegalAccessException | InvocationTargetException e) {
+                    e.printStackTrace();
+                }
             });
         }
     }
